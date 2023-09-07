9fd48ff4c9f889c4c43b6dda910c1ff95480c4ab
yujinago <138287741+yujinago@users.noreply.github.com>
2023-09-07 08:11:38 +0000

    add-users-controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def confirm
+  end
+  
+  def update
+  end
+  
+  def destroy
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  # homes
   root to: 'homes#top'
   get 'about' => 'homes#about'
+  
+  # devise
   devise_for :users
+  
+  # users
+  get 'users/mypage' => 'users#show'
+  get 'users/infomation/edit' => 'users#edit'
+  get 'users/confirm' => 'users#confirm'
+  patch 'users/infomation' => 'users#update'
+  delete 'users/infomation' => 'users#destroy'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
