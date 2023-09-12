059445d93aeb4431768653e9a28399bfde7f0a83
yujinago <138287741+yujinago@users.noreply.github.com>
2023-09-12 11:49:37 +0000

    ホットペッパーAPIの導入&ジャンルの保存機能

--- app/controllers/restaurant_genres_controller.rb
+++ app/controllers/restaurant_genres_controller.rb
@@ -6,9 +6,13 @@ class RestaurantGenresController < ApplicationController
     @restaurant_genres = []
     key = ENV['HotPepper_API_KEY']
     uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/genre/v1/?key=#{key}&format=json")
+    response = Net::HTTP.get_response(uri)
+    if response.code =="301"
+      uri = URI.parse(response["location"])
+    end
     json = Net::HTTP.get(uri)
-    results = JSON.parse(json)
-    results.each do |result|
+    result_all = JSON.parse(json)
+    result_all["results"]["genre"].each do |result|
       restaurant_genre = RestaurantGenre.new(eat(result))
       @restaurant_genres << restaurant_genre
     end
@@ -20,9 +24,9 @@ class RestaurantGenresController < ApplicationController
     redirect_to new_restaurant_path
 
   end
-  
+
   private
-  
+
   def eat(result)
     name = result["name"]
     genre_code = result["code"]
@@ -31,5 +35,5 @@ class RestaurantGenresController < ApplicationController
       genre_code: genre_code
     }
   end
-  
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,66 @@
 class RestaurantsController < ApplicationController
-  def new
-  end
+  require 'net/http'
+  require 'uri'
+  require 'json'
   
-  def create
-    
+  def new
+    @restaurant = Restaurant.new
+    @restaurant_genres = RestaurantGenre.all
   end
 
   def confirm
+    @restaurant_genres = RestaurantGenre.all
+    results = []
+    if params[:restaurant][:select_restaurant_genre_id] == "0"
+      genre_code = params[:restaurant][:restaurant_genre_id]
+    elsif params[:restaurant][:select_restaurant_genre_id] == "1"
+      genre_code = @restaurant_genres.offset( rand(@restaurant_genres.count) ).first.genre_code
+    end
+    if genre_code.blank?
+      redirect_to new_restaurant_path, notice: "条件を選択するか、完全におまかせを選択してください。"
+    else
+      restaurant_genre = RestaurantGenre.find_by(genre_code: genre_code)
+      
+      key = ENV['HotPepper_API_KEY']
+      uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=#{key}&genre=#{genre_code}&format=json")
+      response = Net::HTTP.get_response(uri)
+      if response.code =="301"
+        uri = URI.parse(response["location"])
+      end
+      json = Net::HTTP.get(uri)
+      result_all = JSON.parse(json)
+      result_all["results"]["shop"].each {|n| results << n}
+      
+      result = results.sample
+      result_hash = {
+        user_id: current_user.id,
+        restaurant_genre_id: restaurant_genre.id,
+        name: result["name"],
+        url: result["urls"]["pc"],
+        address: result["address"],
+        open_time: result["open"],
+        legular_holiday: result["close"]
+      }
+      @restaurant_image = result["photo"]["pc"]["l"]
+      @restaurant = Restaurant.new(result_hash)
+    end
   end
-
+  
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    require 'open-uri'
+    restaurant_image = params[:restaurant][:restaurant_image]
+    file = URI.open(restaurant_image)
+    file_name = File.basename(URI.parse(restaurant_image).path)
+    @restaurant.restaurant_image.attach(io: file, filename: file_name)
+    @restaurant.save
+    session[:last_saved_info] = @restaurant
+    redirect_to complete_restaurants_path
+  end
+  
   def complete
+    @restaurant = session[:last_saved_info]
+    @restaurant_genre = RestaurantGenre.find_by(id: @restaurant['restaurant_genre_id'])
   end
 
   def index
@@ -28,4 +79,11 @@ class RestaurantsController < ApplicationController
   def destory
     
   end
+  
+  private
+  
+  def recipe_params
+    params.require(:restaurant).permit(:user_id, :name, :url, :restaurant_genre_id, :address, :open_time, :legular_holiday)
+  end
+  
 end
