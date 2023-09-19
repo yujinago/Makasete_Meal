ef7b8ceb32e8d24a7d1a232fedf925bdf762428d
yujinago <138287741+yujinago@users.noreply.github.com>
2023-09-19 10:47:04 +0000

    restaurant-search-all

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,28 +1,50 @@
 class SearchesController < ApplicationController
   def search
     @recipe_categories = RecipeCategory.all
+    @restaurant_genres = RestaurantGenre.all
     @range = params[:range]
     @word = params[:word]
     @search = params[:search]
     if @range == "Recipe"
-      recipes = Recipe.looks(@search, @word, current_user.id)
+      results = Recipe.looks(@search, @word, current_user.id)
     else
-      @restaurants = current_user.restaurants.looks(@search, @word)
+      results = Restaurant.looks(@search, @word, current_user.id)
     end
     
-    recipes = params[:latest] ? recipes.latest : recipes
-    recipes = params[:old] ? recipes.old : recipes
-    recipes = params[:star_count] ? recipes.star_count : recipes
+    results = results.latest if params[:latest]
+    results = results.old if params[:old]
+    results = results.star_count if params[:star_count]
     
-    @recipes = recipes.page(params[:page])
+    @results = results.page(params[:page])
     
-    search_text_mapping = {
-      "name" => "レシピ名",
-      "poster_name" => "投稿者名",
-      "cook_time" => "調理時間",
-      "cost" => "費用",
-      "foodstuff_name" => "材料名",
-    }
+    # recipes = params[:latest] ? recipes.latest : recipes
+    # recipes = params[:old] ? recipes.old : recipes
+    # recipes = params[:star_count] ? recipes.star_count : recipes
+    
+    # @recipes = recipes.page(params[:page])
+    
+    # restaurants = params[:latest] ? restaurants.latest : restaurants
+    # restaurants = params[:old] ? restaurants.old : restaurants
+    # restaurants = params[:star_count] ? restaurants.star_count : restaurants
+    
+    # @restaurants = restaurants.page(params[:page])
+    if @range == "Recipe"
+      search_text_mapping = {
+        "name" => "レシピ名",
+        "poster_name" => "投稿者名",
+        "cook_time" => "調理時間",
+        "cost" => "費用",
+        "foodstuff_name" => "材料名",
+      }
+    else
+      search_text_mapping = {
+        "name" => "店名",
+        "address" => "住所",
+        "open_time" => "営業時間",
+        "legular_holiday" => "定休日",
+      }
+    end
+    
     @search_text = search_text_mapping[@search]
     
   end
