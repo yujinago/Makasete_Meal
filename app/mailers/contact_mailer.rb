b656756f9d098a577fa320c8f55134a58bcfc18e
yujinago <138287741+yujinago@users.noreply.github.com>
2023-10-10 01:20:43 +0000

    fix-design

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def send_mail(contact, user)
     @contact = contact
-    mail to: user.email, bcc: ENV["GMAIL_USERNAME"], subject: "お問い合わせについて【自動送信】"
+    mail to: user.email, bcc: ENV["GMAIL_USERNAME"], subject: "お問い合わせについて【Makasete Meal(自動送信)】"
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact include ActiveModel::Model
   attr_accessor :nickname, :email, :content
-   
+  
   validates :nickname, presence: true
   validates :email, presence: true
   validates :content, presence: true
