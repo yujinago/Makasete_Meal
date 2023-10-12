2d879eb72a9496f8198c2f2dca2acd5d77a81086
yujinago <138287741+yujinago@users.noreply.github.com>
2023-10-12 03:54:22 +0000

    user-system-rspec

--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -1,5 +1,10 @@
-def check_button(link_text, path)
-  button = find_link(link_text, href: path)
+def check_link(link_text, path)
+  link = find_link(link_text, href: path)
+  expect(link).to be_present
+  expect(link[:href]).to eq(path)
+end
+
+def check_button(button_text)
+  button = find('input[type="submit"][value="' + button_text + '"]')
   expect(button).to be_present
-  expect(button[:href]).to eq(path)
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -22,19 +22,74 @@ RSpec.describe Users, type: :system do
         expect(page).to have_content 'test1@example.com'
       end
       it '登録情報の編集するボタンの表示とURLを検証' do
-        check_button('編集する', users_infomation_edit_path)
+        check_link('編集する', users_infomation_edit_path)
       end
       it '2つのお気に入りボタンの表示とURLを検証' do
-        check_button('お気に入り', recipes_path(user_id: @user.id))
-        check_button('お気に入り', restaurants_path(user_id: @user.id))
+        check_link('お気に入り', recipes_path(user_id: @user.id))
+        check_link('お気に入り', restaurants_path(user_id: @user.id))
       end
       it '2つの一覧を見るボタンの表示とURLを検証' do
-        check_button('一覧を見る', recipes_path)
-        check_button('一覧を見る', restaurants_path)
+        check_link('一覧を見る', recipes_path)
+        check_link('一覧を見る', restaurants_path)
       end
       it 'users_mypage_pathが"/users/mypage"であるか' do
         expect(current_path).to eq('/users/mypage')
       end
+    end
+  end
+  
+  describe 'ユーザー情報編集画面(users_infomation_edit_path)のテスト' do
+    before do
+      visit users_infomation_edit_path
+    end
+    context '表示の確認' do
+      it '編集内容を保存ボタンの表示を検証' do
+        check_button('編集内容を保存')
+      end
+      it '退会するボタンの表示とURLを検証' do
+        check_link('退会する', users_confirm_path)
+      end
+      it 'users_infomation_edit_pathが"/users/infomation/edit"であるか' do
+        expect(current_path).to eq('/users/infomation/edit')
+      end
+    end
+    it '編集内容を保存ボタンがきちんと動作することの確認' do
+      fill_in 'user[nickname]', with: '新しい名前'
+      fill_in 'user[email]', with: 'new@example.com'
+      click_button '編集内容を保存'
+      expect(current_path).to eq(users_mypage_path)
+      expect(page).to have_content('新しい名前')
+      expect(page).to have_content('new@example.com') 
+    end
+  end
+  
+  describe 'ユーザー退会確認画面(users_confirm_path)のテスト' do
+    before do
+      visit users_confirm_path
+    end
+    context '表示の確認' do
+      it '退会しないボタンの表示とURLを検証' do
+        check_link('退会しない', users_mypage_path)
+      end
+      it '退会するボタンの表示とURLを検証' do
+        check_link('退会する', users_infomation_path)
+      end
+      it 'users_confirm_pathが"/users/confirm"であるか' do
+        expect(current_path).to eq('/users/confirm')
+      end
+    end
+    it '退会するボタンがきちんと動作することの確認' do
+      click_link '退会する'
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('退会処理を実行しました。')
+      
+      visit new_user_session_path
+      fill_in 'user[email]', with: @user.email
+      fill_in 'user[password]', with: @user.password
+      click_button 'ログイン', class: "btn btn-outline-info"
+      expect(page).to have_content('Eメールまたはパスワードが違います。')
+    
+      expect(User.count).to eq(0)
     end
   end
   
